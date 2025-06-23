e9b98da43febc3bfeb4786ee29da5089ff24ec7f
muraliuidev <muralikonapala.uidev@gmail.com>
2025-06-23 15:08:38 +0530

    custom validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -11,8 +11,8 @@ type FormValues = {
 
 function YouTubeForm() {
 const form = useForm<FormValues>()
-const { register, control, handleSubmit } = form;
-
+const { register, control, handleSubmit, formState } = form;
+const { errors } = formState;
 
 const onSubmit = (data: FormValues) => {
     console.log('Form Submitted', data)
@@ -22,16 +22,47 @@ const onSubmit = (data: FormValues) => {
     return(
         <div>
             <h1>YouTube Form({renderCount/2})</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="username">User Name</label>
-                {/* <input type="text" id="username" name={name} ref={ref} onChange={onChange} onBlur={onBlur} /> */}
-                <input type="text" id="username" {...register("username")} />
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                <div className="form__control">
+                    <label htmlFor="username">User Name</label>
+                    {/* <input type="text" id="username" name={name} ref={ref} onChange={onChange} onBlur={onBlur} /> */}
+                    <input type="text" id="username" {...register("username", {
+                        required: {
+                            value: true,
+                            message: "Username is required"
+                        }
+                    })} />
+                    <p className="error__msg">{errors.username?.message}</p>
+                </div>
 
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" {...register("email")} />
+                <div className="form__control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" {...register("email",{
+                        pattern: {
+                            value:/^\S+@\S+\.\S+$/,
+                            message: 'Invalid Email'
+                        },
+                        validate: {
+                            notAdmin: (fieldValue) => {
+                                return(
+                                    fieldValue !== "admin@gmail.com" || "Enter a diff email Address"
+                                );
+                            }
+                        }
+                    })} />
+                    <p className="error__msg">{errors.email?.message}</p>
+                </div>
 
-                <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register("channel")} />
+                <div className="form__control">
+                    <label htmlFor="channel">Channel</label>
+                    <input type="text" id="channel" {...register("channel", {
+                        required: {
+                            value: true,
+                            message: 'Channel Required'
+                        }
+                    })} />
+                    <p className="error__msg">{errors.channel?.message}</p>
+                </div>
 
                 <button>Submit</button>
             </form>
